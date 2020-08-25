6c5c07022cc2b65b63925619cfefc1d3528772b4
VARSAP <70198930+VARSAP@users.noreply.github.com>
2020-08-25 15:02:38 +0530

    Add files via upload

---
+++ Divisibilitytest.py
@@ -0,0 +1,11 @@
+for i in range(100,0,-1):
+    if(i%3==0):
+        print(str(i) +"-Agile")
+    elif(i%5==0):
+        print(str(i) +"-Software")
+    if(i%15==0):
+        print(str(i) + "-Testing")
+    else:
+       print(i)
+         
+        
